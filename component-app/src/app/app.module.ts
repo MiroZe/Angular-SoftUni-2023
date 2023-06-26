7646c60b2d720c793911654d363aa8cb584b278a
MiroZe <miro_sv@yahoo.com>
2023-06-26 15:00:34 +0300

    delete unnecessary

--- component-app/src/app/app.module.ts
+++ component-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { EditToDoComponent } from './edit-to-do/edit-to-do.component';
+
 
 
 @NgModule({
@@ -13,7 +13,7 @@ import { EditToDoComponent } from './edit-to-do/edit-to-do.component';
     AppComponent,
     HeaderComponent,
     ToDoListComponent,
-    EditToDoComponent,
+   
   
     
   ],
